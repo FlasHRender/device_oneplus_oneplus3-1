797d06d0d8f2fe5f069f96de489beb21d95de236
Adarsh-MR <adarshmr1998@gmail.com>
2017-10-28 21:39:11 +0000

    op3: cleanup config.fs
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- config.fs
+++ config.fs
@@ -1,9 +1,3 @@
-[system/bin/wcnss_filter]
-mode: 0755
-user: AID_BLUETOOTH
-group: AID_BLUETOOTH
-caps: BLOCK_SUSPEND
-
 [system/bin/cnss-daemon]
 mode: 0755
 user: AID_SYSTEM
@@ -33,15 +27,3 @@ mode: 0755
 user: AID_BLUETOOTH
 group: AID_BLUETOOTH
 caps: BLOCK_SUSPEND
-
-[system/vendor/bin/wcnss_filter]
-mode: 0755
-user: AID_BLUETOOTH
-group: AID_BLUETOOTH
-caps: BLOCK_SUSPEND
-
-[system/bin/cnss-daemon]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
